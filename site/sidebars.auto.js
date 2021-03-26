2bfbbf43cac261647ff6027087a3bbed548f241e
nickthorpe71 <nickthorpe71@gmail.com>
2021-03-26 16:13:36 -0500

    update

--- site/sidebars.auto.js
+++ site/sidebars.auto.js
@@ -1,15 +1,7 @@
 
-        module.exports = {
-            docs: 
-            [
-    "a-copy-2",
-    "a-copy",
-    "test-4",
-    "test-5",
-    "test-6",
-    "test-7",
-    "test-8",
-    "test"
-]
-        };
-        
+module.exports = {
+    docs:
+        [
+            "test"
+        ]
+};
