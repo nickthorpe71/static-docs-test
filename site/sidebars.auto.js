86ec343cdfef138343cd22b20d2459bb9f644a1f
nickthorpe71 <nickthorpe71@gmail.com>
2021-03-29 14:29:20 -0500

    automated commit

--- site/sidebars.auto.js
+++ site/sidebars.auto.js
@@ -2,11 +2,11 @@ module.exports = {docs: [
     "a-copy-2",
     "a-copy",
     "test-10",
-    "test-12",
     "test-4",
     "test-5",
     "test-6",
     "test-7",
     "test-8",
-    "test-9"
+    "test-9",
+    "test"
 ]};
