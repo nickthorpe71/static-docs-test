81599a0ed5cf0e68f48221fc74cdcb4b01b3b2a0
nickthorpe71 <nickthorpe71@gmail.com>
2021-03-30 15:40:53 -0500

    automated commit

--- site/docs/demo-test.md
+++
@@ -1,5 +0,0 @@
----
-title: DEMO
----
-
-TEST CONTENT
--- site/sidebars.auto.js
+++ site/sidebars.auto.js
@@ -1,9 +1,3 @@
 module.exports = {docs: [
-    "a-copy",
-    "demo-test",
-    "test-4",
-    "test-5",
-    "test-6",
-    "test-7",
-    "test"
+    "getting-started"
 ]};
